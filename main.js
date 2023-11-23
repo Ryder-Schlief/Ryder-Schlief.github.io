bf6cee2b6164adcbaf255768e72388339578b93e
Ryder-Schlief <ryderschlief@skiff.com>
2023-11-23 14:36:12 -0800

    added "works" and "about" sections
    they're mostly empty but the framework is there

--- main.js
+++ main.js
@@ -1,25 +1,125 @@
 
 const canvas = document.getElementById("canvas");
 
-const color = `rgb(150, 150, 150)`;
+const color = `rgb(125, 125, 125)`;
 
 var h = innerHeight;
 var w = innerWidth;
+
+var offset = {
+	x: 0,
+	y: 0
+}
 
 var center = {
 	x: w / 2,
 	y: h / 2
 }
 
-const size = 400;
+const size = 500;
 
-const offset = 10;
+const arcOffset = 10;
 
 const arcs = 15;
 
+var zoom = 0;
+
 var ctx = canvas.getContext("2d");
 
-window.onload = window.onresize = function(){
+var home = document.getElementById("home-container");
+var works = document.getElementById("works-container");
+var about = document.getElementById("about-container");
+var navbar = document.getElementById("navbar");
+var navWorks = document.getElementById("nav-works");
+var navHighlight = document.getElementById("nav-highlight");
+var navAbout = document.getElementById("nav-about");
+
+window.onload = function() {
+	canvas.classList.add("visible");
+	updateDivs(true);
+	updateCanvas();
+}
+
+window.onresize = function() {
+	updateCanvas();
+}
+
+window.onhashchange = function() {
+	updateDivs(false);
+}
+
+function updateDivs(instant) {
+	var duration = 750;
+	if (instant) {
+		duration = 0;
+	}
+
+	home.classList.remove("visible");
+	works.classList.remove("visible");
+	about.classList.remove("visible");
+	navWorks.classList.remove("selected");
+	navHighlight.classList.remove("selected");
+	navAbout.classList.remove("selected");
+
+	if (window.location.hash == "#works") {
+		navWorks.classList.add("selected");
+		smoothTransition({ x: 0, y: -h/1.25 }, 1.5, duration);
+		document.title = "Ryder Schlief - Works";
+		setTimeout(addVisible, duration/1.75, works);
+		setTimeout(addVisible, duration/1.75, navbar);
+	} else if (window.location.hash == "#about") {
+		navAbout.classList.add("selected");
+		smoothTransition({ x: -w/1.75, y: -h/1.75 }, 2.25, duration);
+		document.title = "Ryder Schlief - About";
+		setTimeout(addVisible, duration/1.75, about);
+		setTimeout(addVisible, duration/1.75, navbar);
+	} else {
+		smoothTransition({ x: 0, y: 0 }, 1, duration);
+		document.title = "Ryder Schlief - Home";
+		setTimeout(addVisible, duration/1.75, home);
+		navbar.classList.remove("visible");
+	}
+}
+
+function addVisible(element) {
+	element.classList.add("visible");
+}
+
+function smoothTransition(targetOffset, targetZoom, duration) {
+	const startOffset = { x: offset.x, y: offset.y };
+	const startZoom = zoom;
+	var startTime = null;
+  
+	requestAnimationFrame(updateOffset);
+
+	function updateOffset(timestamp) {
+		if (!startTime) {
+			startTime = timestamp;
+		}
+
+		const time = (timestamp - startTime) / duration;
+		const progress = curve(time);
+
+		if (progress < 1) {
+			offset.x = startOffset.x + (targetOffset.x - startOffset.x) * progress;
+			offset.y = startOffset.y + (targetOffset.y - startOffset.y) * progress;
+			zoom = startZoom + (targetZoom - startZoom) * progress;
+
+			requestAnimationFrame(updateOffset);
+		} else {
+			offset = targetOffset;
+			zoom = targetZoom;
+		}
+	}
+}
+
+function curve(x){
+	// cubic bezier curve
+	const y = Math.pow((1 - x), 3) * 0 + Math.pow(3*(1 - x), 2) * x * 0.4 + 3*(1 - x) * x*x * 1 + x*x*x * 1;
+	return y;
+}
+
+function updateCanvas() {
 	canvas.height = innerHeight;
 	canvas.width = innerWidth;
 	h = innerHeight;
@@ -43,14 +143,17 @@ function loop(timeNow) {
 	ctx.clearRect(0, 0, w, h);
 
 	ctx.strokeStyle = color;
-	ctx.lineWidth = 1;
+	ctx.lineWidth = 1 * zoom;
 
 	for (let i = 0; i < arcs; i++) {
-		const arcRadius = offset + ((size - offset) / (arcs-1)) * i;
+		const arcRadius = (arcOffset + ((size - arcOffset) / (arcs-1)) * i) * zoom;
+
+		var x = center.x + offset.x;
+		var y = center.y + offset.y;
 
 		// draw arc
 		ctx.beginPath();
-		ctx.arc(center.x, center.y, arcRadius, 0, Math.PI*2);
+		ctx.arc(x, y, arcRadius, 0, Math.PI*2);
 		ctx.stroke()
 
 
@@ -61,12 +164,12 @@ function loop(timeNow) {
 		angle = modDistance;
 		//angle = angle % Math.PI*2;
 
-		var x = center.x + arcRadius * Math.cos(angle);
-		var y = center.y + arcRadius * Math.sin(angle);
+		x = x + arcRadius * Math.cos(angle);
+		y = y + arcRadius * Math.sin(angle);
 
 		ctx.fillStyle = color;
 		ctx.beginPath();
-		ctx.arc(x, y, 3.5, 0, Math.PI*2);
+		ctx.arc(x, y, 3 * zoom, 0, Math.PI*2);
 		ctx.fill();
 	}
 
